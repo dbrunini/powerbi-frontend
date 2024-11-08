7a0bdb439c36ac60631ab9fb058ff0e78790986a
dbrunini <dbrunini@gmail.com>
2024-11-08 17:05:04 -0300

    Update login.js

--- api/login.js
+++ api/login.js
@@ -1,8 +1,13 @@
 export default function handler(req, res) {
     // Configurações de CORS para permitir o frontend do GitHub Pages
     res.setHeader("Access-Control-Allow-Origin", "https://dbrunini.github.io");
-    res.setHeader("Access-Control-Allow-Methods", "POST");
+    res.setHeader("Access-Control-Allow-Methods", "POST, OPTIONS");
     res.setHeader("Access-Control-Allow-Headers", "Content-Type");
+
+    if (req.method === "OPTIONS") {
+        res.status(200).end(); // Responde a pré-verificações do CORS
+        return;
+    }
 
     // Verificação de autenticação
     const { username, password } = req.body;
