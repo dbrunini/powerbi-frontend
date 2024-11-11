a4cffb32cdc86e8965ecf7aea637f6859a800bb4
dbrunini <dbrunini@gmail.com>
2024-11-11 17:09:48 -0300

    Update login.js

--- api/login.js
+++ api/login.js
@@ -1,14 +1,22 @@
 export default function handler(req, res) {
+    // Adicione o cabeçalho CORS para permitir o domínio do GitHub Pages
     res.setHeader("Access-Control-Allow-Origin", "https://dbrunini.github.io");
     res.setHeader("Access-Control-Allow-Methods", "POST");
     res.setHeader("Access-Control-Allow-Headers", "Content-Type");
 
+    if (req.method === "OPTIONS") {
+        // Responde a requisições OPTIONS antes do POST para o CORS pré-vôo
+        res.status(200).end();
+        return;
+    }
+
+    // Dados de autenticação
     const { username, password } = req.body;
     const validUser = process.env.USERNAME;
     const validPassword = process.env.PASSWORD;
 
+    // Verifica se o usuário e a senha estão corretos
     if (username === validUser && password === validPassword) {
-        // Retorne HTML seguro para o frontend que exibe o conteúdo do Power BI
         const powerBiHtml = `
             <iframe src="${process.env.POWER_BI_LINK}" 
                     style="width: 100%; height: 600px; border: none;">
