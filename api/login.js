461bff8d9141f47e2dfe2592126f15c371c13e41
dbrunini <dbrunini@gmail.com>
2024-11-11 21:33:52 -0300

    Update login.js

--- api/login.js
+++ api/login.js
@@ -1,28 +1,27 @@
-export default function handler(req, res) {
-    // Adicione o cabeçalho CORS para permitir o domínio do GitHub Pages
+export default async function handler(req, res) {
+    // CORS para permitir apenas o domínio do GitHub Pages
     res.setHeader("Access-Control-Allow-Origin", "https://dbrunini.github.io");
     res.setHeader("Access-Control-Allow-Methods", "POST");
     res.setHeader("Access-Control-Allow-Headers", "Content-Type");
 
     if (req.method === "OPTIONS") {
-        // Responde a requisições OPTIONS antes do POST para o CORS pré-vôo
         res.status(200).end();
         return;
     }
 
-    // Dados de autenticação
     const { username, password } = req.body;
     const validUser = process.env.USERNAME;
     const validPassword = process.env.PASSWORD;
 
-    // Verifica se o usuário e a senha estão corretos
     if (username === validUser && password === validPassword) {
-        const powerBiHtml = `
-            <iframe src="${process.env.POWER_BI_LINK}" 
-                    style="width: 100%; height: 600px; border: none;">
-            </iframe>
-        `;
-        res.status(200).send(powerBiHtml);
+        try {
+            // Realiza o fetch do Power BI
+            const response = await fetch(process.env.POWER_BI_LINK);
+            const data = await response.text();
+            res.status(200).send(data);
+        } catch (error) {
+            res.status(500).json({ error: "Erro ao buscar o relatório" });
+        }
     } else {
         res.status(401).json({ success: false });
     }
