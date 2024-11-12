31a12cd960b0a3848f7c2779eaa103c13a3e1dad
dbrunini <dbrunini@gmail.com>
2024-11-11 21:54:04 -0300

    Update login.js

--- api/login.js
+++ api/login.js
@@ -1,27 +1,17 @@
-export default async function handler(req, res) {
-    // CORS para permitir apenas o domínio do GitHub Pages
+export default function handler(req, res) {
+    // Cabeçalhos para permitir CORS
     res.setHeader("Access-Control-Allow-Origin", "https://dbrunini.github.io");
     res.setHeader("Access-Control-Allow-Methods", "POST");
     res.setHeader("Access-Control-Allow-Headers", "Content-Type");
 
-    if (req.method === "OPTIONS") {
-        res.status(200).end();
-        return;
-    }
-
     const { username, password } = req.body;
+
+    // Credenciais
     const validUser = process.env.USERNAME;
     const validPassword = process.env.PASSWORD;
 
     if (username === validUser && password === validPassword) {
-        try {
-            // Realiza o fetch do Power BI
-            const response = await fetch(process.env.POWER_BI_LINK);
-            const data = await response.text();
-            res.status(200).send(data);
-        } catch (error) {
-            res.status(500).json({ error: "Erro ao buscar o relatório" });
-        }
+        res.status(200).json({ success: true, link: process.env.POWER_BI_LINK });
     } else {
         res.status(401).json({ success: false });
     }
